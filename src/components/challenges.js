5227248d73b591f0821c689f7f25a2acb6d71933
Peter Njuguna <peter.njuguna@kirkonulkomaanapu.fi>
2022-06-29 16:19:01 +0300

    adding state

--- src/components/challenges.js
+++ src/components/challenges.js
@@ -3,25 +3,41 @@ import React from 'react';
 import "./App.css";
 
 function Challenge(){
-    let thingsArray = ['I love JS','HTML is used for hacking'];
-    const itemsArray =thingsArray.map(arrayItem=> <h3> {arrayItem}</h3>)
+    // function greetings(personName){
+    //     const date=new Date();
+    //     let hours=date.getHours();
+    //     console.log(hours)
+    //     if(hours>=4&&hours<12){
+    //         console.log( `Good morning ${personName}`)
+    //     }else if(hours>=12&&hours<17){
+    //         console.log( `Good afternoon ${personName}`)
+    //     }else if(hours>=17&&hours<20){
+    //         console.log( `Good evening ${personName}`)
+    //     }else
+    //         console.log( `Good night ${personName}`)
+        
+    //  }
+    // greetings("Peter")
+
+    const results=React.useState("Yes")
+    const [result, setResult]=results
     
+    
+    console.log(result);
 
-
-    function addAryItems(){
-   
-   thingsArray.push(`I have never hacked ${thingsArray.length + 1}`)
-   console.log(thingsArray);
-
-
-}
+    function handleClick(){
+        return(
+            setResult('No')
+        )
+    }
 
     return(
         <div className="container">
-        <div className="col m-3 ">
+        <div onClick={handleClick} className="col m-3 ">
         
-            <button onClick={addAryItems} className="btn btn-primary m-2">Click Me</button>
-            {itemsArray}
+          <h1 className="bg-primary text-center rounded-circle p-5" style={{width:'10rem', height:'10rem'}}>
+          {result}</h1>
+            
         </div>
         </div>
     )
