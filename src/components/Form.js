c8fae9e0f124413b2ec1822cc69c67a08620d779
Peter Njuguna <peter.njuguna@kirkonulkomaanapu.fi>
2022-07-14 12:56:38 +0300

    fetching data from an external api using react useEffect

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,9 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 
 import { FaImage } from 'react-icons/fa';
 
 import memesData from './data/memesData';
+import './App.css';
 
 // import meme1 from './meme-1.png';
 
@@ -14,26 +15,44 @@ function Form(){
         randomImage:'http://i.imgflip.com/1bij.jpg',
     })
 
+    const [allMemes,setAllMemes]= useState([])
+
+    useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes").then((res)=>res.json().then((meme)=>setAllMemes( meme.data.memes)))
+    },[])
     const [allMemeImages,setAllMemeImages] = useState(memesData)
 
     function getMemeImage(){
-        const dataArray =allMemeImages.data.memes
-  const randomMeme= dataArray[Math.floor(Math.random() * dataArray.length)];
+        
+  const randomMeme= allMemes[Math.floor(Math.random() * allMemes.length)];
   const {url}= randomMeme
   setMeme(prevMeme=>({...prevMeme,
     randomImage: url
 }))
  
     }
+
+    function handleChange(event) {
+        const {name,value,type,checked}=event.target
+        setMeme(prevFormData=>{
+            return{
+                ...prevFormData,
+                [name] : type==="checkbox" ?checked : value
+            }
+        })
+    }
+
     return(
     <div className="container">
 
-<input type="text" className="form-input" placeholder='Shut up' ></input>
-<input type="text" className="form-input" placeholder='and take my money'></input>
+<input type="text" className="form-input" placeholder='Shut up' value={meme.topText} name="topText" onChange={handleChange} ></input>
+<input type="text" className="form-input" placeholder='and take my money' value={meme.bottomText} name="bottomText" onChange={handleChange} ></input>
 <div className="d-grid">
  <button className='btn btn-info btn-meme' onClick={getMemeImage}>Get a new meme image <FaImage/></button>
  <div  className="d-img" >
     <img className="" style={{width: '100%',}}  src={meme.randomImage} alt=""></img>
+    <h2 className="meme--text top">{meme.topText}</h2>
+    <h2 className="meme--text bottom">{meme.bottomText}</h2>
 </div>
 </div>
 
--- src/components/challenges/challenges.js
+++ src/components/challenges/challenges.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react"
+import React, {useState,useEffect} from "react"
 
 // import boxes from "./boxes";
 
@@ -11,30 +11,30 @@ import jokesData from "../data/jokesData";
 
 // import image1 from "./../img-1.jpg";
 
-const boxes=[  {
-    id: 1,
-    on: true
-},   
-{
-    id: 2,
-    on: false
-},   
-{
-    id: 3,
-    on: true
-},   
-{
-    id: 4,
-    on: true
-},   
-{
-    id: 5,
-    on: false
-},   
-{
-    id: 6,
-    on: false
-},]
+// const boxes=[  {
+//     id: 1,
+//     on: true
+// },   
+// {
+//     id: 2,
+//     on: false
+// },   
+// {
+//     id: 3,
+//     on: true
+// },   
+// {
+//     id: 4,
+//     on: true
+// },   
+// {
+//     id: 5,
+//     on: false
+// },   
+// {
+//     id: 6,
+//     on: false
+// },]
 
 
 
@@ -77,97 +77,117 @@ export default function Challenge() {
     //     }))
     // }
 
-    const [formData,setFormData]= useState({
-        firstName:"",
-        lastName:"",
-        email:"",
-        comments:"",
-        isFriendly:false,
-        employment:"",
-        favColor:"",
+    // ? State objects
+    // const [formData,setFormData]= useState({
+    //     firstName:"",
+    //     lastName:"",
+    //     email:"",
+    //     comments:"",
+    //     isFriendly:false,
+    //     employment:"",
+    //     favColor:"",
     
-    }
-    )
+    // }
+    // )
 
    
 
-    function handleChange(event) {
-        const {name,value,type,checked}=event.target
-        setFormData(prevFormData=>{
-            return{
-                ...prevFormData,
-                [name] : type==="checkbox" ?checked : value
-            }
-        })
-    }
+    // function handleChange(event) {
+    //     const {name,value,type,checked}=event.target
+    //     setFormData(prevFormData=>{
+    //         return{
+    //             ...prevFormData,
+    //             [name] : type==="checkbox" ?checked : value
+    //         }
+    //     })
+    // }
 
-    function handleSubmit(event) {
-        event.preventDefault()
-        console.log(formData);
-    }
+    // function handleSubmit(event) {
+    //     event.preventDefault()
+    //     console.log(formData);
+    // }
+
+    const [data,setData]= useState({})
+    const [count,setCount]= useState(1)
+
+    useEffect(() => {
+        // console.log("Effect function called");
+        fetch(`https://swapi.dev/api/people/${count+1}`).then(res=>res.json()).then(data=>setData(data))
+    },[count]
+    // use a dependency array to control whether react should rerun
+    )
 
 
     
 
     
     return (
-    <form onSubmit={handleSubmit}>
-    <div className="m-3">
-        <input type="text" placeholder="First Name" value={formData.firstName}    name="firstName"  onChange={handleChange} />
-        </div>
-        <div className="m-3">
-        <input type="text" placeholder="Last Name" value={formData.lastName}       name="lastName"  onChange={handleChange} />
-        </div>
-        <div className="m-3">
-        <input type="email" placeholder="Email" name="email" 
-        value={formData.email}  onChange={handleChange} />
-        </div>
-        <div className="m-3">
-        <textarea name="comments"
-        placeholder="Write your comments here...."
-        value={formData.comments}
-            onChange={handleChange}
-        />
+        // & Fetching data from an API endpoint
+        // anything that is outside of the react scope is considered a side-effect
+        <div>
+        <p>The count is {count}</p>
+        <button onClick={()=>setCount(count+1)}>Get the next character</button>
+            <pre>{JSON.stringify(data, null, 2)}</pre>
 
         </div>
-        <div className="m-3">
-        <input type="checkbox"  id="isFriendly" 
-        checked={formData.isFriendly} name="isFriendly" onChange={handleChange} />
-        <label htmlFor="isFriendly">Would you like to receive promotional emails?</label>
-        </div>
-        <div className="m-3">
-            <fieldset className="d-grid">
-                <legend>Current Employment Status</legend>
-                <input type="radio" id="unemployed" name="employment" value="unemployed"
-                    onChange={handleChange} checked={formData.employment==="unemployed"}
-                />
-                <label htmlFor="unemployed">Unemployed</label>
-                <input type="radio" id="part-time" name="employment" value="part-time"
-                    onChange={handleChange} checked={formData.employment==="part-time"}
-                />
-                <label htmlFor="part-time">Part Time</label>
-                <input type="radio" id="full-time" name="employment" value="full-time"
-                    onChange={handleChange} checked={formData.employment==="full-time"}
-                />
-                <label htmlFor="full-time">Full Time</label>
-            </fieldset>
-        </div>
-        <div>
-            <label htmlFor="best-color">Select Favorite Color</label>
-            <select name="favColor" id="best-color" onChange={handleChange}
-            value={formData.favColor}
-            >
-            <option value="">--Select--</option>
-                <option value="red">Red</option>
-                <option value="orange">Orange</option>
-                <option value="maroon">Maroon</option>
-                <option value="purple">Purple</option>
-                <option value="white">White</option>
-            </select>
-            <br />
-            <button>Submit</button>
-        </div>
-    </form>
+        // & React Form
+    // <form onSubmit={handleSubmit}>
+    // <div className="m-3">
+    //     <input type="text" placeholder="First Name" value={formData.firstName}    name="firstName"  onChange={handleChange} />
+    //     </div>
+    //     <div className="m-3">
+    //     <input type="text" placeholder="Last Name" value={formData.lastName}       name="lastName"  onChange={handleChange} />
+    //     </div>
+    //     <div className="m-3">
+    //     <input type="email" placeholder="Email" name="email" 
+    //     value={formData.email}  onChange={handleChange} />
+    //     </div>
+    //     <div className="m-3">
+    //     <textarea name="comments"
+    //     placeholder="Write your comments here...."
+    //     value={formData.comments}
+    //         onChange={handleChange}
+    //     />
+
+    //     </div>
+    //     <div className="m-3">
+    //     <input type="checkbox"  id="isFriendly" 
+    //     checked={formData.isFriendly} name="isFriendly" onChange={handleChange} />
+    //     <label htmlFor="isFriendly">Would you like to receive promotional emails?</label>
+    //     </div>
+    //     <div className="m-3">
+    //         <fieldset className="d-grid">
+    //             <legend>Current Employment Status</legend>
+    //             <input type="radio" id="unemployed" name="employment" value="unemployed"
+    //                 onChange={handleChange} checked={formData.employment==="unemployed"}
+    //             />
+    //             <label htmlFor="unemployed">Unemployed</label>
+    //             <input type="radio" id="part-time" name="employment" value="part-time"
+    //                 onChange={handleChange} checked={formData.employment==="part-time"}
+    //             />
+    //             <label htmlFor="part-time">Part Time</label>
+    //             <input type="radio" id="full-time" name="employment" value="full-time"
+    //                 onChange={handleChange} checked={formData.employment==="full-time"}
+    //             />
+    //             <label htmlFor="full-time">Full Time</label>
+    //         </fieldset>
+    //     </div>
+    //     <div>
+    //         <label htmlFor="best-color">Select Favorite Color</label>
+    //         <select name="favColor" id="best-color" onChange={handleChange}
+    //         value={formData.favColor}
+    //         >
+    //         <option value="">--Select--</option>
+    //             <option value="red">Red</option>
+    //             <option value="orange">Orange</option>
+    //             <option value="maroon">Maroon</option>
+    //             <option value="purple">Purple</option>
+    //             <option value="white">White</option>
+    //         </select>
+    //         <br />
+    //         <button>Submit</button>
+    //     </div>
+    // </form>
     
 
     )
